bdfcc23c2c0383415ad71228c7670fd222f32ad5
Haitai Ng <nghaitai@msu.edu>
2016-10-04 14:14:49 -0400

    Add project files.

---
+++ ProjectOne/ProjectOne.h
@@ -0,0 +1,35 @@
+
+// ProjectOne.h : main header file for the ProjectOne application
+//
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "include 'stdafx.h' before including this file for PCH"
+#endif
+
+#include "resource.h"       // main symbols
+
+
+// CProjectOneApp:
+// See ProjectOne.cpp for the implementation of this class
+//
+
+class CProjectOneApp : public CWinApp
+{
+public:
+	CProjectOneApp();
+
+
+// Overrides
+public:
+	virtual BOOL InitInstance();
+	virtual int ExitInstance();
+
+// Implementation
+
+public:
+	afx_msg void OnAppAbout();
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CProjectOneApp theApp;
